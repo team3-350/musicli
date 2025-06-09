add9d356d4d280e0d13650472b1bbf63d7e3a206
churuzzz <ruizramos04@outlook.com>
2025-06-09 14:35:39 -0400

    add auth-page and it's auto nav

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,33 +1,39 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import "react-native-reanimated";
 
-import { useColorScheme } from '@/components/useColorScheme';
+import { useColorScheme } from "@/components/useColorScheme";
+import { StatusBar } from "expo-status-bar";
 
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
-} from 'expo-router';
+} from "expo-router";
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: 'auth-page',
+  initialRouteName: "auth-page",
 };
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
+// used to manage the state of the auth for debugging at the moment
+const isAuthed = false;
+
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
     ...FontAwesome.font,
   });
-
-  const isAuthed = false;
 
   // Expo Router uses Error Boundaries to catch errors in the navigation tree.
   useEffect(() => {
@@ -44,10 +50,6 @@ export default function RootLayout() {
     return null;
   }
 
-  if (!isAuthed) {
-    return <AuthPage/>;
-  }
-
   return <RootLayoutNav />;
 }
 
@@ -55,22 +57,26 @@ function RootLayoutNav() {
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-      </Stack>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <StatusBar style={colorScheme === "dark" ? "light" : "dark"} />
+      <DisplayStack />
     </ThemeProvider>
   );
 }
 
-function AuthPage() {
-  const colorScheme = useColorScheme();
+// split between showing login or not
+function DisplayStack() {
+  if (!isAuthed) {
     return (
-      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-        <Stack>
-          <Stack.Screen name="auth-page" options={{ headerShown: false }} />
-        </Stack>
-      </ThemeProvider>
-    )
+      <Stack>
+        <Stack.Screen name="auth-page" options={{ headerShown: false }} />
+      </Stack>
+    );
+  }
+  return (
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="modal" options={{ presentation: "modal" }} />
+    </Stack>
+  );
 }
--- app/auth-page.tsx
+++ app/auth-page.tsx
@@ -1,61 +1,70 @@
-import { StyleSheet, TouchableHighlight } from 'react-native'
-import { Text, View } from '@/components/Themed'
-import { Alert, Button } from 'react-native';
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { StyleSheet, TouchableHighlight } from "react-native";
+import { Text, View } from "@/components/Themed";
+import { Alert, Button } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { useColorScheme } from "@/components/useColorScheme.web";
+
+const colorScheme = useColorScheme();
 
 export default function AuthPage() {
-    return (
-        <SafeAreaProvider>
-            <SafeAreaView>
-                <View style={styles.header}>
-                    <Text style={styles.welcome}>Welcome to Musicli</Text>
-                    <Text style={styles.subtitle}>Link an Account</Text>
-                </View>
-                <TouchableHighlight
-                    onPress={() => Alert.alert('Spotify Auth')}
-                    style={styles.touchable}
-                    underlayColor={"white"}
-                >
-                    <Text style={styles.text}>Spotify</Text>
-                </TouchableHighlight>
-                <TouchableHighlight
-                    onPress={() => Alert.alert('Apple Music Auth')}
-                    style={styles.touchable}
-                >
-                    <Text style={styles.text}>Apple Music</Text>
-                </TouchableHighlight>
-                <TouchableHighlight
-                    onPress={() => Alert.alert('YouTube Music Auth')}
-                    style={styles.touchable}>
-                        <Text style={styles.text}>YouTube Music</Text>
-                    </TouchableHighlight>
-            </SafeAreaView>
-        </SafeAreaProvider>
-    );
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.welcome}>Welcome to Musicli</Text>
+          <Text style={styles.subtitle}>Link an Account</Text>
+        </View>
+        <TouchableHighlight
+          onPress={() => Alert.alert("Spotify Auth")}
+          style={styles.touchable}
+          underlayColor={"white"}
+        >
+          <Text style={styles.text}>Spotify</Text>
+        </TouchableHighlight>
+        <TouchableHighlight
+          onPress={() => Alert.alert("Apple Music Auth")}
+          style={styles.touchable}
+        >
+          <Text style={styles.text}>Apple Music</Text>
+        </TouchableHighlight>
+        <TouchableHighlight
+          onPress={() => Alert.alert("YouTube Music Auth")}
+          style={styles.touchable}
+        >
+          <Text style={styles.text}>YouTube Music</Text>
+        </TouchableHighlight>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
 }
 
 const styles = StyleSheet.create({
-    header: {
-        alignItems: 'center',
-        margin: 30,
-    },
-    welcome: {
-        fontSize: 30,
-        fontWeight: 'bold',
-    },
-    subtitle: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        margin: 20,
-    },
-    touchable: {
-        margin: 20,
-        alignItems: 'center',
-        borderRadius: 10,
-        padding: 10,
-        backgroundColor: '#ddd',
-    },
-    text: {
-        fontSize: 30,
-    }
+  container: {
+    backgroundColor: colorScheme === "dark" ? "rgba(255,255,255,0.1)" : "#eee",
+    flex: 1,
+  },
+  header: {
+    alignItems: "center",
+    margin: 30,
+    backgroundColor: "rgba(0,0,0,0)",
+  },
+  welcome: {
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  subtitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+    margin: 20,
+  },
+  touchable: {
+    margin: 20,
+    alignItems: "center",
+    borderRadius: 10,
+    padding: 10,
+    backgroundColor: "#ddd",
+  },
+  text: {
+    fontSize: 30,
+  },
 });
